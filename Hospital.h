450cec9fdacbc3c89700e6c5c00fbe0d70f39ed6
A01707721 <150314726+A01707721@users.noreply.github.com>
2024-05-22 20:32:55 -0600

    Update Hospital.h

--- Hospital.h
+++ Hospital.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>
 #include "Doctor.h"
 
 //Clase Hospital
@@ -17,7 +18,9 @@ class Hospital{
     int num_p;
     public:
     //Constructor default
-    Hospital():name(""){};
+    Hospital():name(""),num_d(0),num_p(0){};
+    //Constructor
+    Hospital(std::string n):name(n),num_d(0),num_p(0){};
     //Getters y Setters
     std::string get_name();
     Doctor get_d(int);
@@ -25,6 +28,8 @@ class Hospital{
     void set_name(std::string);
     void set_d(Doctor &);
     void set_p(Personal &);
+    void employed_Doctors();
+    void employed_Personal();
 };
 std::string Hospital::get_name(){
     return name;
@@ -43,4 +48,17 @@ void Hospital::set_p(Personal &Per){
     Pe[num_p]=Per;
     num_p++;
 }
+void Hospital::employed_Doctors(){
+    std::cout << name << " " << "Doctors:" << std::endl;
+    for(int i=0;i<num_d;i++){
+        std::cout << i << ".- " << D[i].to_stream() << "payment: " << D[i].pay() << std::endl;
+    }
+}
+void Hospital::employed_Personal(){
+    std::cout << name << " " << "Personal:" << std::endl;
+    for(int i=0; i<num_p;i++)
+        std::cout << i << ".- " << Pe[i].to_stream() << std::endl;
+    
+}
 #endif
+
