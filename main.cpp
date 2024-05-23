dd0cc65273226f094fa595df86e49d80fb27da23
A01707721 <150314726+A01707721@users.noreply.github.com>
2024-05-22 20:33:50 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,40 +1,72 @@
 #include <iostream>
 #include <string>
-#include "Hospital.h"
 #include "Doctor.h"
-void menu(){
-    std::cout << "Treatment" << std::endl;
-    std::cout << "1. Yes" << std::endl;
-    std::cout << "2. No" << std::endl;
+#include "Hospital.h"
+//Menu inicial
+void menu_0(){  
+    std::cout << std::endl;  
+    std::cout << "Hospital" << std::endl;
+    std::cout << "1. Doctors" << std::endl;
+    std::cout << "2. Personal" << std::endl;
+    std::cout << "3. Patients" << std::endl;
+    std::cout << "4. Exit" << std::endl;
 }
+
+//main
 int main(){
+    //Variables para navegar los menus
+    bool cond=true;
     int n=0;
-    Personal Clark;
-    Clark.set_name("Clark");
-    Clark.set_age(43);
-    Clark.set_hours(31);
-    Clark.set_job(1);
-    Doctor John;
-    John.set_name("John");
-    John.set_age(35);
-    John.set_hours(56);
-    John.set_specialty("General MD");
-    std::cout << John.get_name() << " " << John.get_specialty() << std::endl;
-    Patient Ana;
-    Ana.set_name("Ana");
-    Ana.set_age(26);
-    Ana.set_status("Sick");
-    John.set_patient(Ana);
-    std::cout << "Patient " << John.get_patient(0).get_name() << " status: " << John.get_patient(0).get_status() << std::endl;
-    menu();
-    std::cin >> n;
-    if(n==1){
-        John.treatment(0);
-        std::cout << "Patient " << John.get_patient(0).get_name() << " status: " << John.get_patient(0).get_status() << std::endl;
-        std::cout << John.get_patient(0).get_name() << " Medical bill: " << John.get_patient(0).bill() << std::endl;
+    int n_0=0;
+    int n_1=0;
+    //Creacion del objeto tipo Hospital
+    Hospital Sunny("Sunny Hospital"); 
+    //Creacion del objeto tipo Personel
+    Personal Clark_Pe;
+    Clark_Pe.set_name("Clark");
+    Clark_Pe.set_age(43);
+    Clark_Pe.set_hours(31);
+    Clark_Pe.set_job(1);
+    //Creacion de dos objetos tipo Patient
+    Doctor John_Dr("John",35,56);
+    Doctor Angela_Dr("Angela", 43, 48,"Cardiologist");
+    //Creacion de tres objetos tipo Patient
+    Patient Ana_Pa("Ana",24);
+    Patient Greg_Pa("Greg",65,"Medical Discharge");
+    Patient Mark_Pa("Mark",36,"ICU");
+    //Agregacion de objetos tipo Patient a objetos tipo Doctor
+    John_Dr.set_patient(Ana_Pa);
+    John_Dr.set_patient(Greg_Pa);
+    Angela_Dr.set_patient(Mark_Pa);
+    //Agregacion de objetos tipo Doctor y Personel a objeto tipo Hospital
+    Sunny.set_p(Clark_Pe);
+    Sunny.set_d(John_Dr);
+    Sunny.set_d(Angela_Dr);
+    //Ciclo while para mostrar el menu inicial
+    while(cond==true){
+        menu_0();
+        std::cout << "Select: " << std::endl;
+        std::cin >> n_0;
+        if(n_0==1){
+            //Imprime datos de los objetos tipo Doctor 
+            Sunny.employed_Doctors();
+        }
+        if(n_0==2){
+            //Imprime datos de los objetos tipo Personel
+            Sunny.employed_Personal();
+        }
+        if(n_0==3){
+            Sunny.employed_Doctors();
+            std::cout << "Select the number of the doctor to see their patients: ";
+            std::cin >> n_1;
+            //Imprime datos de los objetos tipo Patient 
+            Sunny.get_d(n_1).patients();
+        }
+        if(n_0==4){
+            //Rompe el ciclo while
+            cond=false;
+        }
     }
-    else{
-        std::cout << "Patient " << John.get_patient(0).get_name() << " status: " << John.get_patient(0).get_status() << std::endl;
-    }
+    
     return 0;
 }
