4652377be698f4e3cf9530c5274d90ef38f0e391
A01707721 <150314726+A01707721@users.noreply.github.com>
2024-06-05 22:26:10 -0600

    Update Hospital.h

--- Hospital.h
+++ Hospital.h
@@ -1,78 +1,147 @@
+/* 
+* Proyecto Hospital
+* Adrian Marquez Nunez
+* A01707721
+* 5/06/2024
+*/
 #ifndef Hospital_H
 #define Hospital_H
-
+//Bibliotecas
 #include <iostream>
 #include <string>
 #include <sstream>
 #include "Doctor.h"
 
-//Clase Hospital
+/**Clase Hospital que maneja datos del hospital sus empleados y pacientes
+*y da la capacidad de ver la info de los mismos
+*/ 
+
+//Declaracion de la clase Hospital
 class Hospital{
     private:
-    //Atributos
+    //atributos de la clase: nombre del hospital (name), lista de empleados (E), lista de pacientes (Pa)
     std::string name;
-    //Composición por parte de la clase Employee
+    //Composición por parte de la clase Employee (polimorfismo de Doctor y Personal)
     Employee * E[100];
+    //Numero para crear empleados
     int num_e;
-    //Agregación por part de la clase Patient
+    //Agregación por parte de la clase Patient
     Patient Pa[150];
+    //Numero para agregar pacientes
     int num_p;
     public:
-    //Constructor default
+    /**Constructor default 
+    *@param
+    *@return objeto Hospital
+    */
     Hospital():name(""),num_e(0),num_p(0){};
-    //Constructor
+    /**Constructor donde se reciben las variables para los diferentes atributos
+    *@param string n: el nombre del hospital
+    *@return objeto Hospital
+    */
     Hospital(std::string n):name(n),num_e(0),num_p(0){};
-    //Getters y Setters
+    //Getters 
     std::string get_name();
+    Patient get_patient(int);
     void set_name(std::string);
+    //Setters
     void set_d(std::string,int,int);
     void set_d(std::string,int,int,std::string);
     void set_pe(std::string n,int a, int h, int j);
-    Patient get_patient(int);
     void set_patient(Patient &);
-    //Metodos
+    //Metodos para imprimir datos
     void employees();
     void patients();
     void doctors();
     void others();
 };
+
+/**Getter nombre del hospital
+*@param 
+*@return string:nombre 
+*/
 std::string Hospital::get_name(){
     return name;
 }
+
+/**Setter nombre del hospital
+*@param string:nombre 
+*@return 
+*/
 void Hospital::set_name(std::string n){
     name=n;
 }
+
+/**Setter doctor
+*@param string:nombre, int:edad, int:horas trabajadas
+*@return 
+*/
 void Hospital::set_d(std::string n,int a, int h){
     E[num_e]= new Doctor(n,a,h);
     num_e++;
 }
+
+/**Setter doctor con sobrecarga
+*@param string:nombre, int:edad, int:horas trabajadas, string:especialidad medica
+*@return 
+*/
 void Hospital::set_d(std::string n,int a, int h,std::string s){
     E[num_e]= new Doctor(n,a,h,s);
     num_e++;
 }
+
+/**Setter personal
+*@param string:nombre, int:edad, int:horas trabajadas, int:tipo de trabajo
+*@return 
+*/
 void Hospital::set_pe(std::string n,int a, int h, int j){
     E[num_e]= new Personal(n,a,h,j);
     num_e++;
 }
+
+/**Getter doctor
+*@param int:numero de paciente
+*@return Patient:paciente
+*/
 Patient Hospital::get_patient(int n){
     return Pa[n];
 }
+
+/**Setter paaciente
+*@param Patient:paciente
+*@return 
+*/
 void Hospital::set_patient(Patient &Pat){
     Pa[num_p]=Pat;
     num_p++;
 }
+
+/**Imprime datos de los empleados
+*@param
+*@return 
+*/
 void Hospital::employees(){
     std::cout << name << " " << "Employees:" << std::endl;
     for(int i=0;i<num_e;i++){
         std::cout << std::endl << i+1 << ".- " << E[i]->to_stream() << "payment: " << E[i]->pay() << std::endl;
     }
 }
+
+/**Imprime datos de los pacientes
+*@param
+*@return 
+*/
 void Hospital::patients(){
     std::cout << name << " " << "Patients:" << std::endl;
     for(int i=0;i<num_p;i++){
         std::cout << std::endl << i+1 << ".- " << Pa[i].to_string() << std::endl;
     }
 }
+
+/**Imprime datos de los empleados tipo doctor
+*@param
+*@return 
+*/
 void Hospital::doctors(){
     int cont=1;
     std::cout << name << " " << "Doctors:" << std::endl;
@@ -83,6 +152,11 @@ void Hospital::doctors(){
         }   
     }
 }
+
+/**Imprime datos de los empleados tipo personal
+*@param
+*@return 
+*/
 void Hospital::others(){
     int cont=1;
     std::cout << name << " " << "Personal:" << std::endl;
@@ -93,4 +167,5 @@ void Hospital::others(){
         }   
     }
 }
+
 #endif
